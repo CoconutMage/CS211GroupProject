7f13ddfe3544218deb6223153882a323c0b50bf5
WillieGrayson <57761926+WillieGrayson@users.noreply.github.com>
2019-11-17 20:42:34 -0500

    Basic Outline for UI

---
+++ MeetUpScreen.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.event.*;
+public class MeetUpScreen extends JFrame
+{
+  private JSlider slider;
+  private JTextField firstAddress;
+  private JTextField secondAddress;
+  //private Controller controller;
+  /*
+   * Very basic start to screen
+   * Features 2 textFields the user will use to give the two desired endpoints(addresses)
+   * A Calculate or Midpoint button that will give the controller the class both addresses and find the midpoint
+   * This is merely an outline so the appearance and number of features is not final
+   */
+  public MeetUpScreen()
+  {
+    setTitle("Meet Up");
+    setSize(400,400);
+    firstAddress = new JTextField(10);
+    secondAddress = new JTextField(10);
+    JButton midPointButton = new JButton("Midpoint");
+    /* This occurs when the button is pressed
+     */
+    midPointButton.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent e) {
+        /*
+         * Button will give the controller the two address in both textFields and call its run method
+        controller.setAddress(firstAddress.getText(),1);
+        controller.setAdress(secondAddress.getText(),2);
+        controller.run();
+        */
+        // Printers for testing
+        if(firstAddress.getText().equals("") || secondAddress.getText().equals(""))
+          System.out.println("Please enter both addresses!");
+        else
+        {
+          System.out.println("First Address: "+firstAddress.getText());
+          System.out.println("Second Address: "+secondAddress.getText());
+        }
+      }
+    });
+    JPanel panel = new JPanel();
+    panel.setLayout(null);
+    midPointButton.setBounds(150, 100, 100, 50);
+    firstAddress.setBounds(30,50,100,50);
+    secondAddress.setBounds(270,50,100,50);
+    panel.add(midPointButton);
+    panel.add(firstAddress);
+    panel.add(secondAddress);
+    this.getContentPane().add(panel);
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+  }
+  public static void main(String[] args)
+  {
+    MeetUpScreen screen = new MeetUpScreen();
+  } 
+  
+}
