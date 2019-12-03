f7707fea09618370c1c24467fb5c2726221061b6
WillieGrayson <57761926+WillieGrayson@users.noreply.github.com>
2019-12-03 06:06:43 -0500

    Add files via upload

---
+++ Current Presentation Build/Location.java
@@ -0,0 +1,38 @@
+/*
+ * The Location class stores a latitude and longitude attribute.
+ * It is used explicitly when the controller finds a midpoint.
+ * Every MeetingPlace object is also a Location object.
+*/
+
+public class Location {
+ // Declares latitude and longitude attributes.
+ private String lat;
+ private String lng;
+ 
+ // Declares two constructors.
+ public Location() {
+  this.lat = "";
+  this.lng = "";
+ }
+ 
+ public Location(String lat, String lng) {
+  this.lat = lat;
+  this.lng = lng;
+ }
+ 
+ // Declares setters and getters for lat and lng.
+ public String getLat() {
+  return lat;
+ }
+ public void setLat(String lat) {
+  this.lat = lat;
+ }
+ 
+ public String getLng() {
+  return lng;
+ }
+ public void setLng(String lng) {
+  this.lng = lng;
+ }
+ 
+}
---
+++ Current Presentation Build/Step.java
@@ -0,0 +1,50 @@
+
+/* The Step class stores information about each step in the navigation from Address 1 to Address 2.
+ * It is used explicitly as a way for the controller method ParseSteps to store the information
+ * needed to calculate the midpoint.
+ */
+
+public class Step {
+ // Declare attributes.
+ private Location startLoc;
+ private float distance;
+ private int duration;
+ private Location endLoc;
+ 
+ // Declare constructor. There should be no instance of a default Step.
+ public Step(Location startLoc, float distance, int duration, Location endLoc) {
+  this.startLoc = startLoc;
+  this.distance = distance;
+  this.duration = duration;
+  this.endLoc = endLoc;
+ }
+ 
+ // Declares getters only. Step information should not be edited after being constructed.
+ public void setStartLoc(Location startLoc) {
+  this.startLoc = startLoc;
+ }
+ public Location getStartLoc() {
+  return startLoc;
+ }
+ 
+ public void setDistance(float distance) {
+  this.distance = distance;
+ }
+ public float getDistance() {
+  return distance;
+ }
+ 
+ public void setDuration(int duration) {
+  this.duration = duration;
+ }
+ public int getDuration() {
+  return duration;
+ }
+ 
+ public void setEndLoc(Location endLoc) {
+  this.endLoc = endLoc;
+ }
+ public Location getEndLoc() {
+  return endLoc;
+ }
+}
